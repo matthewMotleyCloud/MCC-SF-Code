336b43d9f27c3edd4c8f30bccf8f1ff300fe31d2
matthewMotleyCloud <matthew@motleycloud.co>
2025-06-23 09:45:17 -0500

    Weather component Deer IQ

--- DeerIQApp/force-app/main/default/lwc/weatherComponent/weatherComponent.js
+++ DeerIQApp/force-app/main/default/lwc/weatherComponent/weatherComponent.js
@@ -1,47 +1,61 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import getWeatherData from '@salesforce/apex/WeatherService.getWeatherData';
+import getWeatherSummary from '@salesforce/apex/WeatherService.getWeatherSummary';
 
 const LAT_FIELD = 'Hunt_Area__c.Hunt_Area_Center__Latitude__s';
 const LONG_FIELD = 'Hunt_Area__c.Hunt_Area_Center__Longitude__s';
-
 const FIELDS = [LAT_FIELD, LONG_FIELD];
 
 export default class WeatherComponent extends LightningElement {
     @api recordId;
-    @track weather;
-    @track error;
+    latitude;
+    longitude;
+    latRounded;
+    lonRounded;
+    pointsUrl;
+    observationStationsUrl;
+    firstStationUrl;
+    temperatureF;
+    windDirection;
+    windCompass;
+    windSpeedMph;
+    barometricPressureInches;
+    precipitationLast6HoursInches;
 
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
-    wiredRecord({ error, data }) {
+    wiredRecord({ data }) {
         if (data) {
-            const latitude = getFieldValue(data, LAT_FIELD);
-            const longitude = getFieldValue(data, LONG_FIELD);
-
-            if (latitude && longitude) {
-                getWeatherData({ latitude, longitude })
-                    .then(result => {
-                        const weatherData = JSON.parse(result);
-                        const period = weatherData.properties.periods[0];
-                        this.weather = {
-                            temp: period.temperature + ' ' + period.temperatureUnit,
-                            wind: period.windDirection + ' ' + period.windSpeed,
-                            shortForecast: period.shortForecast,
-                            detailedForecast: period.detailedForecast
-                        };
-                        this.error = undefined;
+            this.latitude = getFieldValue(data, LAT_FIELD);
+            this.longitude = getFieldValue(data, LONG_FIELD);
+            if (this.latitude != null && this.longitude != null) {
+                getWeatherSummary({ latitude: this.latitude, longitude: this.longitude })
+                    .then(summary => {
+                        this.pointsUrl = 'https://api.weather.gov/points/' + this.latitude + ',' + this.longitude;
+                        this.latRounded = summary.latRounded;
+                        this.lonRounded = summary.lonRounded;
+                        this.observationStationsUrl = summary.observationStationsUrl;
+                        this.firstStationUrl = summary.firstStationUrl;
+                        this.temperatureF = summary.temperatureF;
+                        this.windDirection = summary.windDirection;
+                        this.windCompass = summary.windCompass;
+                        this.windSpeedMph = summary.windSpeedMph;
+                        this.barometricPressureInches = summary.barometricPressureInches;
+                        this.precipitationLast6HoursInches = summary.precipitationLast6HoursInches;
                     })
-                    .catch(e => {
-                        this.error = 'Could not fetch weather: ' + (e.body?.message || e.message);
-                        this.weather = undefined;
+                    .catch(() => {
+                        this.pointsUrl = undefined;
+                        this.latRounded = undefined;
+                        this.lonRounded = undefined;
+                        this.observationStationsUrl = undefined;
+                        this.firstStationUrl = undefined;
+                        this.temperatureF = undefined;
+                        this.windDirection = undefined;
+                        this.windCompass = undefined;
+                        this.windSpeedMph = undefined;
+                        this.barometricPressureInches = undefined;
+                        this.precipitationLast6HoursInches = undefined;
                     });
-            } else {
-                this.error = 'No latitude/longitude found on this Hunt Area.';
-                this.weather = undefined;
             }
-        } else if (error) {
-            this.error = 'Could not fetch Hunt Area location.';
-            this.weather = undefined;
         }
     }
 }
